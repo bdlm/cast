31c32569fb8eda7e800fa1f80b6c668c4b8e3c5b
Michael Kenney <michael.kenney@validity.com>
2023-12-23 12:26:25 -0700

    cleanup

--- to.go
+++ to.go
@@ -76,7 +76,6 @@ func ToE[TTo Types](val any, o ...Ops) (panicTo TTo, panicErr error) {
 	switch to.Type().Kind() {
 	// reflect.Array:
 	// reflect.Invalid:
-	// reflect.Map:
 	// reflect.Pointer:
 	// reflect.Struct:
 	// reflect.UnsafePointer:
@@ -113,7 +112,6 @@ func ToE[TTo Types](val any, o ...Ops) (panicTo TTo, panicErr error) {
 		reti, err = toSlice(to, val, size)
 	case reflect.Func:
 		reti, err = toFunc[TTo](to, val)
-		//reti, err = toFunc[TTo](from)
 	case reflect.Complex64:
 		reti, err = toComplex[complex64](from)
 	case reflect.Complex128:
@@ -158,118 +156,3 @@ func ToE[TTo Types](val any, o ...Ops) (panicTo TTo, panicErr error) {
 
 	return ret, errors.WrapE(Error, errors.Errorf("unable to cast %#.10v of type %T to %T", from, from, to.Interface()))
 }
-
-//func toType(typ reflect.Type, from any) (reflect.Value, error) {
-//	var err error
-//	var ret = reflect.New(typ)
-//	var reterr = func(r any, e error) (reflect.Value, error) {
-//		if nil != e {
-//			return ret, e
-//		}
-//		if v, ok := r.(reflect.Value); ok {
-//			return reflect.Append(ret, v), nil
-//		}
-//		return reflect.Append(ret, reflect.ValueOf(r)), nil
-//	}
-//	fmt.Printf("toType: %s\n", typ)
-//
-//	switch typ.Kind() {
-//	case reflect.Invalid:
-//		err = errors.Errorf("toType: unknown type %s (%T)", ret.Type(), ret.Interface())
-//
-//	case reflect.Chan:
-//		fmt.Println("	not implemented")
-//	case reflect.Func:
-//		fmt.Println("	not implemented")
-//	case reflect.Map:
-//		fmt.Println("	not implemented")
-//	case reflect.Pointer:
-//		fmt.Println("	not implemented")
-//	case reflect.Struct:
-//		fmt.Println("	not implemented")
-//	case reflect.UnsafePointer:
-//		fmt.Println("	not implemented")
-//
-//	case reflect.Bool:
-//		ret, err = reterr(toBool(from))
-//	case reflect.Array:
-//		fallthrough
-//	case reflect.Slice:
-//		r, err := toType(ret.Elem().Elem().Type(), from)
-//		if nil != err {
-//			ret = reflect.Append(ret, r)
-//		}
-//
-//	// Numbers
-//	case reflect.Complex64:
-//		ret, err = reterr(toComplex[complex64](reflect.ValueOf(from)))
-//	case reflect.Complex128:
-//		ret, err = reterr(toComplex[complex128](reflect.ValueOf(from)))
-//	case reflect.Float32:
-//		ret, err = reterr(toFloat[float32](reflect.ValueOf(from)))
-//	case reflect.Float64:
-//		ret, err = reterr(toFloat[float64](reflect.ValueOf(from)))
-//	case reflect.Int:
-//		ret, err = reterr(toInt[int](reflect.ValueOf(from)))
-//	case reflect.Int8:
-//		ret, err = reterr(toInt[int8](reflect.ValueOf(from)))
-//	case reflect.Int16:
-//		ret, err = reterr(toInt[int16](reflect.ValueOf(from)))
-//	case reflect.Int32:
-//		ret, err = reterr(toInt[int32](reflect.ValueOf(from)))
-//	case reflect.Int64:
-//		ret, err = reterr(toInt[int64](reflect.ValueOf(from)))
-//	case reflect.Uint:
-//		ret, err = reterr(toInt[uint](reflect.ValueOf(from)))
-//	case reflect.Uint8:
-//		ret, err = reterr(toInt[uint8](reflect.ValueOf(from)))
-//	case reflect.Uint16:
-//		ret, err = reterr(toInt[uint16](reflect.ValueOf(from)))
-//	case reflect.Uint32:
-//		ret, err = reterr(toInt[uint32](reflect.ValueOf(from)))
-//	case reflect.Uint64:
-//		ret, err = reterr(toInt[uint64](reflect.ValueOf(from)))
-//	case reflect.Uintptr:
-//		ret, err = reterr(toInt[uintptr](reflect.ValueOf(from)))
-//		//	case reflect.Complex64:
-//		//		fallthrough
-//		//	case reflect.Complex128:
-//		//		fallthrough
-//		//	case reflect.Float32:
-//		//		fallthrough
-//		//	case reflect.Float64:
-//		//		fallthrough
-//		//	case reflect.Int:
-//		//		fallthrough
-//		//	case reflect.Int8:
-//		//		fallthrough
-//		//	case reflect.Int16:
-//		//		fallthrough
-//		//	case reflect.Int32:
-//		//		fallthrough
-//		//	case reflect.Int64:
-//		//		fallthrough
-//		//	case reflect.Uint:
-//		//		fallthrough
-//		//	case reflect.Uint8:
-//		//		fallthrough
-//		//	case reflect.Uint16:
-//		//		fallthrough
-//		//	case reflect.Uint32:
-//		//		fallthrough
-//		//	case reflect.Uint64:
-//		//		fallthrough
-//		//	case reflect.Uintptr:
-//		//		ret, err = toInt[TTo](from)
-//
-//	case reflect.Interface:
-//		ret = reflect.ValueOf(interface{}(from))
-//
-//	case reflect.String:
-//		ret = reflect.ValueOf(fmt.Sprintf("%#v", from))
-//	}
-//
-//	fmt.Printf("toType: %#v (%T); %#v (%T)\n", ret, ret, err, err)
-//	return ret, err
-//
-//}
