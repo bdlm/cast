21b4d7e293281950d39fda61f1f831ff062c488d
Michael Kenney <mkenney@webbedlam.com>
2025-11-03 13:14:48 -0700

    module updates

--- to.go
+++ to.go
@@ -78,11 +78,11 @@ func ToE[TTo Types](val any, ops ...Op) (panicTo TTo, panicErr error) {
 	default:
 		retIface = ret0Val
 		if _, ok := retIface.(error); ok {
-			retIface = errors.Errorf(To[string](val, ops...))
+			retIface = errors.Errorf("%s", To[string](val, ops...))
 		} else if _, ok := retIface.(std_error.Error); ok {
-			retIface = errors.Errorf(To[string](val, ops...))
+			retIface = errors.Errorf("%s", To[string](val, ops...))
 		} else if _, ok := retIface.(fmt.Stringer); ok {
-			retIface = errors.Errorf(To[string](val, ops...))
+			retIface = errors.Errorf("%s", To[string](val, ops...))
 		} else {
 			return ret0Val, errors.WrapE(Error, errors.Errorf(ErrorStrUnableToCast, from, from.Interface(), to.Interface()))
 		}
